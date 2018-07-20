a72d89d58fa48e6482729bceee7eaccafa76e9ef
Rafael Epplee <raffomania@gmail.com>
2018-07-20 12:27:14 +0200

    fix butterfly rotation

--- scenes/butterfly/butterfly.gd
+++ scenes/butterfly/butterfly.gd
@@ -1,14 +1,27 @@
 extends Sprite
 
 var time = 0
+var original_rotation
+
+func _ready():
+    original_rotation = rotation
+
 func _process(dt):
     time += dt * 5
     var dx = sin(time + 2) + sin(time * 0.1) * 0.3
     if dx > 0:
-        # global_scale = Vector2(-1, 1)
-        set_flip_h(true)
+        flip('right')
     else:
-        # global_scale = Vector2(1, 1)
-        set_flip_h(true)
+        flip('left')
     translate(Vector2(dx, sin(sin(time) + sin(time * 0.3) + sin(time * 2))))
-    # rotate(sin(time * 0.5) * 0.01)
+    rotate(sin(time * 0.2) * 0.01)
+
+func flip(side):
+    if side == 'right' and not flip_h:
+        set_flip_h(true)
+        rotation = - rotation
+        pass
+    elif side == 'left' and flip_h:
+        set_flip_h(false)
+        rotation = - rotation
+
