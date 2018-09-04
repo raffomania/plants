fb4fb65b0a9710879c43b4ff8dfdf73339c691e8
Rafael Epplee <raffomania@gmail.com>
2018-09-04 21:37:24 +0200

    work better on all screen sizes

--- scenes/sun/sun.gd
+++ scenes/sun/sun.gd
@@ -1,5 +1,12 @@
 extends Sprite
 
+export(Color) var sun_up
+export(Color) var sun_down
+export(float) var speed
+
 func _process(dt):
-    rotate(dt * 0.1)
+    rotate(dt * speed)
+    var norm_rotation = fmod(rotation, 2 * PI)
+    var distance_from_top = min(norm_rotation, (2 * PI) - norm_rotation) / PI
+    get_node('/root/root/bg').material.set_shader_param('color', sun_up.linear_interpolate(sun_down, distance_from_top))
 
