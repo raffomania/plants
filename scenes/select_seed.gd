94564a5293cce4550ef2fb32ea4efbc43742cf13
Rafael Epplee <raffomania@gmail.com>
2018-09-22 11:24:19 +0200

    fix some stuff

--- scenes/select_seed.gd
+++ scenes/select_seed.gd
@@ -1,31 +1,31 @@
 extends ScrollContainer
 
 func _ready():
-        var plants = get_available_plants()
-        for scene in plants:
-                var button = Button.new()
-                button.text = 'treeeee'
-                button.connect('pressed', self, 'plant_selected', [scene])
-                $HBoxContainer.add_child(button)
+	var plants = get_available_plants()
+	for scene in plants:
+		var button = Button.new()
+		button.text = 'treeeee'
+		button.connect('pressed', self, 'plant_selected', [scene])
+		$HBoxContainer.add_child(button)
 
 func plant_selected(scene):
-        print(scene)
-        var plant = scene.instance()
-        $'../garden'.add_child(plant)
+	print(scene)
+	var plant = scene.instance()
+	$'../garden'.add_child(plant)
 
 func get_available_plants():
-        var scenes = []
+	var scenes = []
 
-        var dir = Directory.new()
-        dir.open('res://scenes/plants')
+	var dir = Directory.new()
+	dir.open('res://scenes/plants')
 
-        dir.list_dir_begin()
-        while true:
-                var path = dir.get_next()
-                if path == '':
-                        break
-                else:
-                        scenes.append(load(path))
-        dir.list_dir_end()
+	dir.list_dir_begin()
+	while true:
+		var path = dir.get_next()
+		if path == '':
+			break
+		elif path != '.' and path != '..':
+			scenes.append(load('res://scenes/plants/' + path))
+	dir.list_dir_end()
 
-        return scenes
+	return scenes
